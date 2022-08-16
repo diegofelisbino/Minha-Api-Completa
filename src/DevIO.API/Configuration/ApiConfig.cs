caccd1181febf263aa9cdb051c3dc56c73411a99
Diego Felisbino <diego.felisbino@outlook.com>
2022-08-16 15:43:07 -0300

    Criação da classe ApiConfig

---
+++ src/DevIO.API/Configuration/ApiConfig.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfig(this IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddControllers();
+
+            services.AddMvc();
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiConfig(this IApplicationBuilder app, IWebHostEnvironment env)
+        {   
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+           
+            
+
+            return app;
+        }
+    }
+}
--- src/DevIO.API/Program.cs
+++ src/DevIO.API/Program.cs
@@ -5,9 +5,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-
 builder.Services.AddDbContext<MeuDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -17,39 +14,16 @@ builder.Services.AddIdentityConfiguration(builder.Configuration);
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-builder.Services.AddMvc();
-
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.SuppressModelStateInvalidFilter = true;
-});
-
-//DependencyInjectionConfig.ResolveDependencies(builder.Services);
-builder.Services.ResolveDependencies();
-
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApiConfig();
 
 builder.Services.AddSwaggerGen();
 
+builder.Services.ResolveDependencies();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthentication();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
+
+app.UseApiConfig(app.Environment);
 
 app.Run();
